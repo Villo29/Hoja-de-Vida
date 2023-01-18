ea284380c5f1ab96fa8f8e5a022610beb4bfc5c0
David Ruiz <jesuruga@gmail.com>
2023-01-18 17:41:06 -0600

    segundo commit

---
+++ src/componens/atoms/header.jsx
@@ -0,0 +1,12 @@
+import "../../assets/styles/header.css"
+
+function Header() {
+    return (  
+        <header>
+
+            <h1>Curriculum vitae</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../componens/atoms/header";
+
+function  App(){
+    return(
+        <>
+        <Header></Header>
+        </>
+        
+    )
+}
+export default App;
