e16ca22ceb7a3dd9dee857b0114a076246d01323
David Ruiz <jesuruga@gmail.com>
2023-01-22 01:30:57 -0600

    [add] opcions

--- src/componens/moleculas/Estudi.jsx
+++ src/componens/moleculas/Estudi.jsx
@@ -5,7 +5,21 @@ import "../../assets/images/qr.png"
 function Estudie() {
     return (
         <div className="estudio">
-
+            <div id="estio">
+              <h2>Estudios</h2>
+            </div>
+            <div id="primaria">
+        Primaria: Primero de Mayo (2008 - 2014)
+            </div>
+            <div id="secundaria">
+            Secundaria: Jose Emilio Grajales (2014-2017) 
+            </div>
+            <div id="prepa">
+            Preparatoria: Cbtis #169 (2017-2020) 
+            </div>
+            <div id="uni">
+            Universidad: Universidad Politecnica de Chiapas (2021-2024)
+            </div>
         </div>
       );
 }
--- src/componens/moleculas/habilidades.jsx
+++ src/componens/moleculas/habilidades.jsx
@@ -5,7 +5,13 @@ import "../../assets/styles/habilidades.css"
 function Habilidades() {
     return ( 
         <div className="habilidades">
-
+            <h1 id="habi">
+            Habilidades
+            </h1>
+            <h1 id="txt2">
+            Facilidad para poder trabajar en equipo, comunicacion oral y escrita, administracion de documentacion,
+            manejo de diferentes tecnologias como NodeJs, ExpresJS 
+            </h1>
         </div>
      );
 }
