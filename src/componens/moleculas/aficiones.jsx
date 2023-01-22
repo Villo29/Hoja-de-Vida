044165a89982eaa747569c8c19d2df7e4af0ba34
David Ruiz <jesuruga@gmail.com>
2023-01-22 12:01:34 -0600

    [add] iconos

--- src/componens/moleculas/aficiones.jsx
+++ src/componens/moleculas/aficiones.jsx
@@ -4,7 +4,11 @@ import "../../assets/styles/aficiones.css"
 function Aficiones() {
     return ( 
         <div className="Aficiones">
-
+            <h1 id="habi">
+            Aficiones
+            </h1>
+            <h1 id="txt3">Me gustas estar informado sobre los nuevos temas 
+            que salen de la tecnologia.</h1>
         </div>
      );
 }
